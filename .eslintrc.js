fb3b81e9100356d52422ffeab939c822825fc2de
ysfscream <894402575@qq.com>
2019-07-26 10:37:13 +0800

    chore(package): rebuilding the project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,17 @@
 module.exports = {
-  env: {
-    browser: true,
-    node: true
-  },
   root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
+  env: {
+    node: true,
   },
-  extends: 'airbnb-base',
-  // required to lint *.vue files
-  plugins: [
-    'html'
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
   ],
-  // check if imports actually resolve
-  'settings': {
-    'import/resolver': {
-      'webpack': {
-        'config': 'build/webpack.base.conf.js'
-      }
-    }
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
-  // add your custom rules here
-  'rules': {
-    // don't require .vue extension when importing
-    'import/extensions': ['error', 'always', {
-      'js': 'never',
-      'vue': 'never'
-    }],
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'semi': 0,
-    'no-shadow': 0,
-    'no-param-reassign': 0,
-    'new-cap': 0,
-    'no-console': 0,
-    'consistent-return': 0,
-    'import/no-extraneous-dependencies': [2, { devDependencies: true }],
-  }
-}
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+};
