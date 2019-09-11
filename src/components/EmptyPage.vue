8afa3f1dca3fe7823f29dbb840a5692041c2a923
CrazyWisdom <zh.whong@gmail.com>
2019-09-11 11:02:29 +0800

    feat(EmptyPage): add EmptyPage.vue

---
+++ src/components/EmptyPage.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="empty-page">
+    <div>
+      <img src="@/assets/images/no-connection.png">
+    </div>
+    <el-button plain>+ New Connection</el-button>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'EmptyPage',
+};
+</script>
+
+
+<style scoped lang="scss">
+@import '@/assets/scss/variable.scss';
+
+.empty-page {
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin-left: 300px;
+  top: 40%;
+  transform: translateY(-50%);
+  -webkit-transform: translateY(-50%);
+  -ms-transform: translateY(-50%);
+  text-align: center;
+  img {
+    width: 400px;
+  }
+  .el-button {
+    margin-top: 32px;
+    border: 2px solid $color-main-green;
+    color: $color-main-green;
+    border-radius: 0;
+    &:hover, &:focus {
+      border-color: $color-second-green;
+      color: $color-second-green;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,31 @@
 <template>
   <div class="home">
     <Leftbar msg="Welcome to Your Vue.js App"/>
+    <div class="right-content">
+      <EmptyPage/>
+    </div>
   </div>
 </template>
 
 
 <script>
-import Leftbar from '@/components/Leftbar.vue';
+import Leftbar from '@/components/Leftbar.vue'
+import EmptyPage from '@/components/EmptyPage.vue'
 
 export default {
   name: 'home',
   components: {
     Leftbar,
+    EmptyPage,
   },
 };
 </script>
+
+
+<style lang="scss">
+.home {
+  .right-content {
+    margin-left: 300px;
+  }
+}
+</style>
