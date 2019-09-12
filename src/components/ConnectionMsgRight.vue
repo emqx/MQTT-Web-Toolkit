c401568b29d0585fbb2d1e82e544c1273b93bf08
CrazyWisdom <zh.whong@gmail.com>
2019-09-12 15:30:32 +0800

    feat(ConnectionContent): add MsgLeft MsgRight MsgPublish

---
+++ src/components/ConnectionMsgRight.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="connection-msg-right">
+    <p class="right-topic">{{ topic }}</p>
+    <div class="right-payload payload">
+      <pre>{{ payload }}</pre>
+    </div>
+    <p class="right-time time">{{ time }}</p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'ConnectionMsgRight',
+  props: {
+    topic: String,
+    payload: String,
+    time: String,
+  },
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import "@/assets/scss/mixins.scss";
+@import '@/assets/scss/variable.scss';
+
+.connection-msg-right {
+  text-align: right;
+  @include msg-item;
+  .right-payload {
+    color: #fff;
+    background: $color-main-green;
+    border-radius: 12px 0 12px 12px;
+    text-align: left;
+  }
+}
+</style>
