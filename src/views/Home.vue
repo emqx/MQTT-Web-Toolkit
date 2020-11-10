1fc5806e5f63f9fffbd7ad79a0dfc4e081f3d38c
LiYingdong <2586632362@qq.com>
2020-11-10 15:44:50 +0800

    feat(test): build test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,75 @@
 <template>
-  <div class="home">
-    <Leftbar/>
-    <div class="right-content">
-      <router-view></router-view>
-    </div>
+  <div class="home-view">
+    <Leftbar />
+    <RouterView />
+    <Ipc @setTheme="setTheme" @setLang="setLang" />
   </div>
 </template>
 
-
-<script>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { Getter } from 'vuex-class'
+import Ipc from '@/components/Ipc.vue'
 import Leftbar from '@/components/Leftbar.vue'
 
-export default {
-  name: 'home',
+@Component({
   components: {
+    Ipc,
     Leftbar,
   },
-  computed: {
-    connections() {
-      return this.$store.state.connections
-    },
-  },
+})
+export default class Home extends Vue {
+  @Getter('currentTheme') private getterTheme!: Theme
+  @Getter('currentLang') private getterLang!: Language
+
+  private setTheme(currentTheme: Theme): void {
+    const bodyTag: HTMLBodyElement | null = document.querySelector('body')
+    if (!bodyTag) {
+      return
+    }
+    bodyTag.className = currentTheme
+  }
+
+  private setLang(currentLang: Language): void {
+    document.documentElement.lang = currentLang
+    this.$i18n.locale = currentLang
+  }
+
+  private created() {
+    this.setTheme(this.getterTheme)
+    this.setLang(this.getterLang)
+  }
 }
 </script>
 
+<style lang="scss" scope>
+@import '~@/assets/scss/mixins.scss';
 
-<style lang="scss">
-@import '@/assets/scss/variable.scss';
-
-.home {
+.home-view {
+  height: 100%;
+  background-color: var(--color-bg-primary);
+  .topbar {
+    @include flex-space-between;
+    min-height: 60px;
+    padding: 0 16px;
+    border-bottom: 1px solid var(--color-border-default);
+  }
+  .right-topbar {
+    position: fixed;
+    left: 341px;
+    right: 0;
+    z-index: 3;
+    background: var(--color-bg-normal);
+  }
+  .rightbar {
+    margin-left: 81px;
+    height: 100%;
+    background-color: var(--color-bg-primary);
+  }
   .right-content {
-    margin-left: $width-leftbar;
+    margin-left: 341px;
+    height: 100%;
+    background-color: var(--color-bg-primary);
   }
 }
 </style>
