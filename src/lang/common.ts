ab45a30c0b56341c8917ca5e55b8bb2546c136d5
Red-Asuka <liyd0623@gmail.com>
2022-06-08 16:18:09 +0800

    feat(empty-page): add EMQX Cloud ads to empty page

--- src/lang/common.ts
+++ src/lang/common.ts
@@ -104,4 +104,12 @@ export default {
     en: 'Advanced Config',
     ja: '詳細設定',
   },
+  cloud: {
+    zh:
+      '需要一个云原生的全托管 MQTT 服务？一键部署 <a href="https://www.emqx.com/zh/cloud/?utm_source=mqttx&utm_medium=app&utm_campaign=cloud" target="_blank" rel="noopener noreferrer">EMQX Cloud</a>！14 天免费试用！',
+    en:
+      'Need a Fully Managed, Cloud-Native MQTT Messaging Service? Try <a href="https://www.emqx.com/en/cloud/?utm_source=mqttx&utm_medium=app&utm_campaign=cloud" target="_blank" rel="noopener noreferrer">EMQX Cloud</a> now! 14-day trial, no credit card required.',
+    ja:
+      'フルマネージドのクラウドネイティブMQTTメッセージングサービスが必要ですか？今すぐ <a href="https://www.emqx.com/en/cloud/?utm_source=mqttx&utm_medium=app&utm_campaign=cloud" target="_blank" rel="noopener noreferrer">EMQX Cloud</a> をお試しください。30日間の試用版で、クレジットカードは必要ありません。',
+  },
 }
